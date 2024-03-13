e412789eed07247df8c9370d8e01d89588169cb8
Muhammad Nauman Raza <devraza@skiff.com>
2024-03-13 21:07:42 +0000

    chore(flake): cleanup

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "Rust development environment for Ambition using fenix";
-
   inputs = {
     fenix = {
       url = "github:nix-community/fenix";
@@ -27,20 +25,15 @@
         toolchain = pkgs.fenix.complete;
       in rec
       {
-        # Executed by `nix build`
         packages.default =
           (pkgs.makeRustPlatform {
-            # Use nightly rustc and cargo provided by fenix for building
             inherit (toolchain) cargo rustc;
           })
           .buildRustPackage {
             pname = "vaporise";
-            version = "0.1.0";
+            version = "0.2.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
-
-            # For other makeRustPlatform features see:
-            # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
           };
 
         # Executed by `nix run`
@@ -48,14 +41,8 @@
 
         # Used by `nix develop`
         devShells.default = pkgs.mkShell rec {
-          # Use nightly cargo & rustc provided by fenix. Add for packages for the dev shell here
-          buildInputs = with pkgs; [
-            (with toolchain; [
-              cargo rustc rust-src clippy rustfmt # rust components
-            ])
-            mold
-            clang
-            pkg-config
+          buildInputs = with pkgs.toolchain; [
+            cargo rustc rust-src clippy rustfmt # rust components
           ];
           # Specify the rust-src path (many editors rely on this)
           RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
