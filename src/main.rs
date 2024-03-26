6516e2f2b89782a07802736d5a2b1416a16ea1c8
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-03-26 13:54:15 +0000

    feat: error handling if removing file fails
    To think this got left out for this long...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use colored::Colorize;
 use std::{fs, path, process};
+use anyhow::{Context, Result};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None, author)]
@@ -14,38 +15,47 @@ struct Args {
     targets: Vec<String>,
 }
 
-fn main() -> std::io::Result<()> {
+fn vaporise() -> Result<()> {
     let args = Args::parse();
 
     if args.targets.is_empty() {
-        println!("{} no arguments passed", "error:".red().bold());
+        println!("{}: no arguments passed", "error".red().bold());
         println!(
-            "{} try 'vpr -h' for more information",
-            "note:".cyan().bold()
+            "{}: try 'vpr -h' for more information",
+            "note".cyan().bold()
         );
         process::exit(0);
     }
 
+
     for target in args.targets.iter() {
         if !args.no_preserve && (target == "/" || target == "~") {
-            println!("{} you're trying to delete an important directory ({})! specify '{}' if you really want to do this", "error:".red().bold(), "--no-preserve".yellow(), target);
+            println!("{}: you're trying to delete an important directory ({})! specify '{}' if you really want to do this", "error".red().bold(), "--no-preserve".yellow(), target);
             process::exit(0);
         }
 
         if path::Path::new(target).exists() {
             if fs::metadata(target).unwrap().is_dir() {
-                let _ = fs::remove_dir_all(target);
+                let _ = fs::remove_dir_all(target)
+                    .with_context(|| format!("could not remove directory: {}", target.bold()))?;
             } else {
-                let _ = fs::remove_file(target);
+                let _ = fs::remove_file(target)
+                    .with_context(|| format!("could not remove file: {}", target.bold()))?;
             }
         } else {
             println!(
-                "{} the specified target does not exist {}",
-                "error:".red().bold(),
+                "{}: the specified target does not exist {}",
+                "error".red().bold(),
                 target.yellow()
             );
         }
     }
 
     Ok(())
+}
+
+fn main() {
+    if let Err(error) = vaporise() {
+        println!("{}: {:?}", "error".red().bold(), error);
+    }
 }
